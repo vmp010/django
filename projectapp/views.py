1998d37181fe70351868ae4abae6e82ebfd1ee67
vmp010 <B11223053@gemail.yuntech.edu.tw>
2023-12-12 10:50:21 +0800

    index html

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from projectapp.models import RecordE,RecordR,CategoryE,CategoryR
 # Create your views here.
+def recordall(request):
+    recordE=RecordE.objects.all()
+    recordR=RecordR.objects.all()
+    return render(request,"index.html",locals())
