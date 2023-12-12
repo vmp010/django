ee1ef4fcbab2ac3c46b59268089b508f294d0905
vmp010 <B11223053@gemail.yuntech.edu.tw>
2023-12-12 19:24:14 +0800

    1212

--- projectapp/views.py
+++ projectapp/views.py
@@ -4,4 +4,13 @@ from projectapp.models import RecordE,RecordR,CategoryE,CategoryR
 def recordall(request):
     recordE=RecordE.objects.all()
     recordR=RecordR.objects.all()
-    return render(request,"index.html",locals())
+    
+    recordAllE=RecordE.objects.filter()
+    recordAllR=RecordR.objects.filter()
+    income_list=[record.cash for record in recordAllR]
+    outcome_list=[record.cash for record in recordAllE]
+    income=sum(income_list) if len(income_list)!=0 else 0
+    outcome=sum(outcome_list) if len(outcome_list)!=0 else 0
+
+    net = income -outcome
+    return render(request,'index.html',locals())
