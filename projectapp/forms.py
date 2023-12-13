a77548927568e736c7f4ad0e3328b30a01fa5a4e
vmp010 <B11223053@gemail.yuntech.edu.tw>
2023-12-13 08:20:24 +0800

    Update forms.py

--- projectapp/forms.py
+++ projectapp/forms.py
@@ -3,3 +3,6 @@ from django import forms
 class addcategoryF(forms.Form):
     addcategoryR=forms.CharField(max_length=20,required=True)
     addcategoryE=forms.CharField(max_length=20,required=True)
+class delcheckbox(forms.Form):
+    delcheckboxR=forms.BooleanField(required=False)
+    delcheckboxE=forms.BooleanField(required=False)
