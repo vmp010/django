15df2d4e9f4d90b3cdc60547ee1263cf15f74b7f
vmp010 <B11223053@gemail.yuntech.edu.tw>
2023-12-13 12:42:46 +0800

    1242

--- projectapp/admin.py
+++ projectapp/admin.py
@@ -4,7 +4,16 @@ from .models import RecordE, CategoryE,RecordR,CategoryR
 
 
 # Register your models here.
+class RecordRAdmin(admin.ModelAdmin):
+    list_filter=('categoryR',)
+    search_fields=('categoryR','date')
+    ordering=('date',)
+class RecordEAdmin(admin.ModelAdmin):
+    list_filter=('categoryE')
+    search_fields=('categoryE','date')
+    ordering=('date',)
+
 admin.site.register(RecordE)
 admin.site.register(CategoryE)
-admin.site.register(RecordR)
+admin.site.register(RecordR,RecordRAdmin)
 admin.site.register(CategoryR)
